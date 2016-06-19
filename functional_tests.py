8838a5d1da80b20077d3bbc65ddc63bfb10e3bd6
zaargh <zaargh@protonmail.ch>
2016-06-19 07:54:29 +0200

    First commit: First FT and basic Django config

---
+++ functional_tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+
+binary = FirefoxBinary('/opt/firefox_42/firefox')
+
+browser = webdriver.Firefox(firefox_binary=binary)
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
