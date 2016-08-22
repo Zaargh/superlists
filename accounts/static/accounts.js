74b9b543aa62357b1bbe4c8b62c2dd5e370fbc6d
zaargh <zaargh@protonmail.ch>
2016-08-22 22:23:33 +0200

    JS for auth

---
+++ accounts/static/accounts.js
@@ -0,0 +1,12 @@
+/* global $ */
+var initialize = function (navigator) {
+    $('#id_login').on('click', function () {
+        navigator.id.request();
+    });
+};
+
+window.Superlists = {
+    Accounts: {
+        initialize: initialize
+    }
+};
