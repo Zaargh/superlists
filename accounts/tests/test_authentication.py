9f63cced635704b963a7fb332e2db3f7543e3c21
zaargh <zaargh@protonmail.ch>
2016-08-23 00:39:20 +0200

    Authentication unit tests pass

--- accounts/tests/test_authentication.py
+++ accounts/tests/test_authentication.py
@@ -1,9 +1,12 @@
 from unittest.mock import patch
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from accounts.authentication import (
     PERSONA_VERIFY_URL, DOMAIN, PersonaAuthenticationBackend
 )
+
+User = get_user_model()
 
 
 @patch('accounts.authentication.requests.post')
@@ -11,6 +14,9 @@ class AuthenticateTest(TestCase):
 
     def setUp(self):
         self.backend = PersonaAuthenticationBackend()
+        user = User(email='other@user.com')
+        user.username = 'otheruser'
+        user.save()
 
     def test_sends_assertion_to_mozilla_with_domain(self, mock_post):
         self.backend.authenticate('an assertion')
@@ -21,7 +27,7 @@ class AuthenticateTest(TestCase):
 
     def test_returns_none_if_response_errors(self, mock_post):
         mock_post.return_value.ok = False
-
+        mock_post.return_value.json.return_value = {}
         user = self.backend.authenticate('an assertion')
         self.assertIsNone(user)
 
@@ -29,3 +35,9 @@ class AuthenticateTest(TestCase):
         mock_post.return_value.json.return_value = {'status': 'not okay!'}
         user = self.backend.authenticate('an assertion')
         self.assertIsNone(user)
+
+    def test_finds_existing_user_with_email(self, mock_post):
+        mock_post.return_value.json.return_value = {'status': 'okay', 'email': 'a@b.com'}
+        actual_user = User.objects.create(email='a@b.com')
+        found_user = self.backend.authenticate('an assertion')
+        self.assertEqual(found_user, actual_user)
