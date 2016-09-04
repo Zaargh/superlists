a22383818d1a4282bf8f990aef3b97d1b9da023d
zaargh <zaargh@protonmail.ch>
2016-09-04 20:00:25 +0200

    fixed fabric commands in test

--- functional_tests/fabfile.py
+++ functional_tests/fabfile.py
@@ -1,15 +1,13 @@
 from fabric.api import env, run
 
-FAB_VENV_PYTHON = '~/fab_venv/bin/python'
-
 
 def _get_base_folder(host):
     return '~/sites/' + host
 
 
 def _get_manage_dot_py(host):
-    return '{python} {path}/source/manage.py'.format(
-        python=FAB_VENV_PYTHON, path=_get_base_folder(host)
+    return '{path}/venv/bin/python {path}/source/manage.py'.format(
+        path=_get_base_folder(host)
     )
 
 
