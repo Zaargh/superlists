e9fa577400f4989f09e7b31eb7b622c0a22978e9
zaargh <zaargh@protonmail.ch>
2016-09-05 19:57:03 +0200

    mock tested NewListForm, skeleton implementation

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
-from lists.forms import ExistingListItemForm, ItemForm
+from lists.forms import ExistingListItemForm, ItemForm, NewListForm
 from lists.models import Item, List
 User = get_user_model()
 
@@ -32,6 +32,14 @@ def new_list(request):
         return redirect(list_)
     else:
         return render(request, 'home.html', {'form': form})
+
+
+def new_list2(request):
+    form = NewListForm(data=request.POST)
+    if form.is_valid():
+        list_ = form.save(owner=request.user)
+        return redirect(list_)
+    return render(request, 'home.html', {'form': form})
 
 
 def my_lists(request, email):
